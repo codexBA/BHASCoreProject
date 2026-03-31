8a9e574b6e8979ea947941f2e0bbed621b770e9a
Erkan Islamovic <erkan@codex.ba>
2026-03-31 15:00:50 +0200

    [UPDATE] Dodali smo Web API - cijene

---
+++ BHASCoreProject/Controllers/API/CijeneController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BHASCore.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CijeneController : ControllerBase
+    {
+        public IActionResult Get()
+        {   
+            return Ok(
+                new
+                {
+                    artikli = new[]
+                    {    
+                        new { naziv = "Narandza", cijena = 1.5 },
+                        new { naziv = "Limunada", cijena = 2.0 },
+                        new { naziv = "Kafa", cijena = 2.0 }
+                    },
+                    konobar = "Aco"
+                }
+            );
+        }
+    }
+} 
